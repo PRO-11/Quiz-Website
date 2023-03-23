d9ba9e5c507dd554385da48b527ac3f5c0dfba44
PRATHAM GUPTA <91843874+PRO-11@users.noreply.github.com>
2023-03-24 00:27:24 +0530

    config vercel

--- Backend/db.js
+++ Backend/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const url=process.env.mongourl
 
 const connectToMongo=()=>{
-   mongoose.connect(url,()=>{
+   mongoose.connect("mongodb+srv://PRATHAM:PRATHAM111@pro-quiz.1tl3a.mongodb.net/test",()=>{
     console.log("Connect to db");
    }) 
 }
